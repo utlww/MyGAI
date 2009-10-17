897c9caf9b4e4d5561201fd88e93b841adf33321
Danny Wang <chaowang05@gmail.com>
2009-10-17 22:15:11 +0800

    update game entity

--- WestWorld1/BaseGameEntity.h
+++ WestWorld1/BaseGameEntity.h
@@ -3,6 +3,18 @@
 
 class GameEntity
 {
+private:
+    int m_ID;
+    static int m_iNextValidID;
 
+    void SetID(int val);
+public:
+    GameEntity(int id) { SetID(id); }
+    
+    virtual ~GameEntity(){};
+    
+    virtual void Update() =0;
+
+    int ID() const {return m_ID;}
 };
 #endif
